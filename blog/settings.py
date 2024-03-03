eb3a22db14066932dfa273ec31c93618c5d2c815
Jakub Stóż <jakubstroz@gmail.com>
2024-03-03 22:55:46 +0100

    congfigure admin panel

--- blog/settings.py
+++ blog/settings.py
@@ -107,8 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'status',
+        'created_by',
+        'create_date',
+        'update_date',
+        'publish_date',
+    )
+
+    list_filter = (
+        'status',
+        'created_by',
+        'create_date',
+        'update_date',
+        'publish_date',
+    )
+
+    search_fields = (
+        'status',
+        'title',
+        'body',
+        'created_by',
+    )
+
+    prepopulated_fields = {'slug':('title',)}
