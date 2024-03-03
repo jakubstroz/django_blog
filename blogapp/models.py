42db5e866ba514994d933dde690944d501d1805a
Jakub Stóż <jakubstroz@gmail.com>
2024-03-03 22:02:54 +0100

    add blogapp, create Post model

---
+++ blogapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+
+STATUS = (
+    (1, 'sketch'),
+    (2, 'published')
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+    slug = models.SlugField(max_length=55,unique=True)
+    body = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=1)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    create_date = models.DateTimeField(auto_now_add= True)
+    update_date = models.DateTimeField(auto_now= True)
+    publish_date = models.DateTimeField(default= now)
+
+    class Meta:
+        ordering = ('-create_date',)
+    
+    def __str__(self) -> str:
+        return self.title
