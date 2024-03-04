83799d80d7c112cc05b6062e7034a7018f5d6742
Jakub Stóż <jakubstroz@gmail.com>
2024-03-04 02:39:20 +0100

    add forms to add/edit post and delete post function

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, create_post, edit_post, delete_post
 app_name = 'blogapp'
 urlpatterns = [
     path('', post_list,name='post_list'),
-    path('post/<slug>/', post_detail, name='post_detail')
+    path('create', create_post, name='create_post'),
+    path('post/<slug>/edit/', edit_post, name='edit_post'),
+    path('post/<slug>/', post_detail, name='post_detail'),
+    path('post/<slug>/delete/', delete_post, name='delete_post'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
+from django.urls import reverse
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(status__in=[1,2])
+    #posts = Post.objects.filter(status__in=[1,2])
+    posts = Post.objects.filter(status=2)
     paginator = Paginator(posts, 2)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -18,3 +21,47 @@ def post_detail(request, slug):
     #post = Post.objects.get(slug=slug)
     context = {'post_detail':post,}
     return render(request,'post.html',context)
+
+def create_post(request):
+    title = "Create"
+    form = PostForm(request.POST or None, request.FILES or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.instance.created_by = request.user
+            form.save()
+            return redirect(reverse("blogapp:post_detail", kwargs={
+                'slug':form.instance.slug,
+                }))
+
+    context = {
+        'title': title,
+        'form': form,
+    }
+    return render(request, 'create_post.html', context)
+
+def edit_post(request, slug):
+    title = "Edit"
+    post = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None,
+                     request.FILES or None,
+                       instance=post)
+    
+    if request.method == 'POST':
+        if form.is_valid():
+            form.instance.created_by = request.user
+            form.save()
+            return redirect(reverse("blogapp:post_detail", kwargs={
+                'slug':form.instance.slug,
+                }))
+
+    context = {
+        'title': title,
+        'form': form,
+    }
+
+    return render(request, 'create_post.html', context)
+
+def delete_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    post.delete()
+    return redirect(reverse('blogapp:post_list'))
